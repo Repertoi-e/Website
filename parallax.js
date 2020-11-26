9bdf9a25b299c7ba17de785a871af38ea40fcecb
Repertoi-e <dsotirov@windowslive.com>
2020-11-26 04:03:09 +0200

    Fix parallax hopefully once and for all lmao

--- parallax.js
+++ parallax.js
@@ -1,16 +1,29 @@
-const parallaxElements = document.querySelectorAll(".parallax");
-const parallaxElementsSpecial = document.querySelectorAll(".parallax_special");
+const contentContainer = document.querySelector(".content_container");
+
+const backgrounds = getComputedStyle(contentContainer).backgroundPosition.replace(/^\s+|\s+$/g,"").split(/\s*,\s*/);
+
+const speeds = [0.2, 0.7, 0.7, 1]; // me, clouds, clouds, bg
 
 window.addEventListener("scroll", () => {
     let scroll = window.pageYOffset;
 
-    parallaxElements.forEach(e => {
-        let speed = e.dataset.speed;
-        e.style.transform = `translate(-50%, -50%) translateY(${scroll * speed}px)`;
+    let result = "";
+
+    backgrounds.forEach((bg, i) => {
+        let coords = bg.replace(/[^0-9-]/g,' ').split(/ +/).slice(0, 2).map(x => Number(x));
+        
+        let speed = speeds[i];
+        
+        result += coords[0] + "px ";
+        result += (coords[1] + scroll * speed) + "px";
+
+        if (i != backgrounds.length - 1) {
+            result += ", "; 
+        }
     });
 
-    parallaxElementsSpecial.forEach(e => {
-        let speed = e.dataset.speed;
-        e.style.transform = `translateY(${scroll * speed}px)`;
-    });
+    console.log(result);
+// background-position: 0px 700px, -300px 0px, 50% 0px, 0px 0px;
+
+    contentContainer.style.backgroundPosition = result;
 });
