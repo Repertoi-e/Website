9013165aa1d7968658c2d77817513ea016b3546f
Repertoi-e <dsotirov@windowslive.com>
2023-05-19 01:26:47 +0300

    Update about page

--- js/about.js
+++ js/about.js
@@ -1,26 +1,27 @@
-// Handle the projects view in the about page
-
-$(".projects__windows__window").find("p").hide();
-
-var $grid = $(".projects__windows").masonry({
-    itemSelector: ".projects__windows__window",
-    ccolumnWidth: ".projects__window__sizer",
-    stamp: ".projects__windows__stamp",
-    percentPosition: true,
-    originLeft: false
-});
-
-$grid.imagesLoaded().progress(function () {
-    $grid.masonry("layout");
-});
-
 $(document).ready(function () {
-    $(".projects__windows__window").hover(function () {
-        $(this).find(".projects__windows__window__overlay").fadeTo(100, 0.5);
+    $(".content__block").find("p").hide();
+
+    var $grid = $(".content").masonry({
+        itemSelector: ".content__block",
+        columnWidth: ".content__block",
+        percentPosition: true,
+        originLeft: false
+    }); 
+
+    $grid.imagesLoaded().progress(function () {
+        $grid.masonry("layout");
+    }); 
+
+    let videoElements = document.querySelectorAll('video');
+    for(let video of videoElements){
+        video.addEventListener('loadeddata', (event) => {
+            $grid.masonry("layout");
+        });
+    }
+
+    $(".content__block").hover(function () {
+        $(this).find(".overlay").fadeTo(100, 0.5);
     }, function () {
-        $(this).find(".projects__windows__window__overlay").fadeTo(100, 0);
+        $(this).find(".overlay").fadeTo(100, 0);
     });
 });
-
-
-
